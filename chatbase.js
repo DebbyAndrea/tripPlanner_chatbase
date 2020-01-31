336dec8bf6e4635ed4af2cec9c08c5c07d94afb7
Debby <DebbyAndrea.J@cognizant.com>
2020-01-31 18:31:55 +0530

    first commit

---
+++ chatbase.js
@@ -0,0 +1,48 @@
+const chatbase = require('@google/chatbase');
+
+module.exports = function sendChatBaseRequest(agent, botResponse, flag) {
+    const messageSet = chatbase.newMessageSet()
+        .setApiKey("5ed3c243-cbcb-416f-b75f-37b2e186a581") // Chatbase API key
+        .setPlatform("myOwmApp")
+        .setVersion('1.0');
+
+    const sessionId = agent.session;
+    const userInput = agent.query;
+    const intentName = agent.intent;
+    let time = Date.now();
+    const botTime = Number(++time);
+
+    const userMessage = messageSet.newMessage() // Create a new instance of Message
+        .setAsTypeUser() // Mark it as a message coming from the human
+        .setUserId(sessionId) // User ID on the chat platform, or custom ID
+        .setTimestamp(Date.now().toString()) // Mandatory
+        .setIntent(intentName) // The intent decoded from the user message, if applicable
+        .setMessage(userInput); // User message
+
+    if (flag) {
+        userMessage.setAsHandled(); // Mark this request as successfully handled ;)
+    } else {
+        userMessage.setAsNotHandled(); // Tell Chatbase to mark this user request as "not handled"
+    }
+    
+    const botMessage = messageSet.newMessage() // See above
+        .setAsTypeAgent() // This message is the bot response
+        .setUserId(sessionId) // Same as above
+        .setTimestamp(botTime.toString()) // Mandatory
+        .setIntent(intentName)
+        .setMessage(botResponse); // Bot response message
+
+    return messageSet.sendMessageSet()
+    .then(response => {
+        var createResponse = response.getCreateResponse();
+        console.log(response)
+        console.log("request done");
+        return createResponse.all_succeeded; // "true" if all messages were correctly formatted and have been successfully forwarded
+    })
+    .catch(error => {
+        console.log("Error: ")
+        console.error(error);
+        return false;
+    });
+
+}
---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const { WebhookClient } = require("dialogflow-fulfillment");
+const chatBase = require("./chatbase");
+
+let app = express();
+
+app.get("/", (req, res) => {
+    res.send("online");
+  });
+
+app.post("/dialogflow", express.json(), (req, res) => {
+    const agent = new WebhookClient({ request: req, response: res });
+  
+    let intentMap = new Map();
+
+    let welcome = () => {
+        let botResponse = "Hi... this is from welcome fulfillment";
+        // chatBase(agent, botResponse, true);
+        agent.add(botResponse);
+    }
+
+    let fallback = () => {
+        let botResponse = "Sorry... this is from fallback fulfillment";
+        // chatBase(agent, botResponse, false);
+        agent.add(botResponse);
+    }
+
+    let bookFlights = () => {
+        let botResponse = `Done! I've booked your tickets for ${agent.parameters.date}, flying from ${agent.parameters.geo_city} to ${agent.parameters.geo_city1}(ff)`;
+        agent.add(botResponse);
+    }
+
+    let bookCars = () => {
+        let botResponse = `I've booked ${agent.parameters.car_type} in ${agent.parameters.geo_city} for ${agent.parameters.date} for you (ff)`;
+        agent.add(botResponse);
+    }
+
+    let bookRooms = () => {
+        let botResponse = `Done! I've booked your room in ${agent.parameters.geo_city} for ${agent.parameters.date} (ff)`;
+        agent.add(botResponse);
+    }
+
+    let bookFlightsCar = () => {
+        let botResponse = `I've booked your car in ${agent.parameters.geo_city} for ${agent.parameters.date}`;
+        agent.add(botResponse);
+    }
+
+    let bookFlightsRoom = () => {
+        let botResponse = `Reserved a room in ${agent.parameters.geo_city} on ${agent.parameters.date}`;
+        agent.add(botResponse);
+    }
+
+    intentMap.set("Default Welcome Intent", welcome);
+    intentMap.set("Default Fallback Intent", fallback);
+    intentMap.set("BookFlights", bookFlights);
+    intentMap.set("BookCars", bookCars);
+    intentMap.set("BookRooms", bookRooms);
+    intentMap.set("BookFlights - followupBookCars", bookFlightsCar);
+    intentMap.set("BookFlights - followupBookRooms", bookFlightsRoom);
+
+    agent.handleRequest(intentMap);
+});
+
+let server = app.listen(8080, () => {
+    console.log("Connected");
+});
+
+module.exports = { app, server };
+
